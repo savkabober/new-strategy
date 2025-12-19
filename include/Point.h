0c60e1f1f7c6f6c1ffac8f9a69d3eeec4c2c6fdc
savkabober <savmalyshev@mail.ru>
2025-12-19 19:57:26 +0300

    всякие функции начал писать в математике

--- include/Point.h
+++ include/Point.h
@@ -8,17 +8,18 @@ using namespace std;
 #define square(x) ((x) * (x))
 
 struct Point
-{   
+{
     public:
     double x, y, z;
-    Point() : x(0), y(0), z(0) {}
-    Point(double x, double y) : x(x), y(y), z(0) {}
-    Point(double x, double y, double z) : x(x), y(y), z(z) {}
+    Point(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) {}
     double mag2() const {
         return x * x + y * y + z * z;
     }
     double mag() const {
         return sqrt(mag2());
+    }
+    double arg() const {
+        return atan2((*this).y, (*this).x);
     }
     Point operator+() const
     {
@@ -42,51 +43,56 @@ struct Point
     }
     Point operator/(const double a) const
     {
-        return Point(x / a, y / a, z / a);
+        return (*this) * (1 / a);
     }
-    double operator*(const Point &p) const
+    Point operator*(const Point &p) const {
+        return Point(y * p.z - z * p.y, z * p.x - x * p.z, x * p.y - y * p.x);
+    }
+    double operator^(const Point &p) const
     {
-        return x * x + y * y + z * z;
+        return x * p.x + y * p.y + z * p.z;
     }
     double operator^(double a) const {
         return pow((*this).mag(), a);
     }
-    Point operator^(const Point &p) const {
-        return Point((*this).y * p.z - (*this).z * p.y, (*this).z * p.x - (*this).x * p.z, (*this).x * p.y - (*this).y * p.x);
+    Point operator+(double angle) const {
+        double len = (*this).mag(), angle0 = (*this).arg();
+        return Point(len * cos(angle0 + angle), len * sin(angle0 + angle));
+    }
+    Point operator-(double angle) const {
+        return (*this) + (-angle);
     }
     Point &operator+=(const Point &p)
     {
-        x += p.x;
-        y += p.y;
-        z += p.z;
+        *this = *this + p;
         return *this;
     }
     Point &operator-=(const Point &p)
     {
-        x -= p.x;
-        y -= p.y;
-        z -= p.z;
+        *this = *this - p;
         return *this;
     }
     Point &operator*=(const double a) {
-        x *= a;
-        y *= a;
-        z *= a;
+        *this = *this * a;
         return *this;
     }
-    Point &operator^=(const Point &p) {
-        Point a = Point((*this).y * p.z - (*this).z * p.y, (*this).z * p.x - (*this).x * p.z, (*this).x * p.y - (*this).y * p.x);
-        *this = a;
+    Point &operator/=(const double a) {
+        *this = *this / a;
         return *this;
     }
-    operator double() {
-        return (*this).mag();
+    Point &operator*=(const Point &p) {
+        *this = *this * p;
+        return *this;
+    }
+    Point &operator+=(const double angle) {
+        *this = *this + angle;
+        return *this;
+    }
+    Point &operator-=(const double angle) {
+        *this = *this - angle;
+        return *this;
     }
     Point unity() const {
-        double len = (*this).mag();
-        if (len == 0) {
-            return Point();
-        }
-        return Point(x / len, y / len, z / len);
+        return (*this) / (*this).mag();
     }
 };
