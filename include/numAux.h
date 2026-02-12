7ae0a6ac07f349ab2ef09d4c92e0e1887e08d339
savkabober <savmalyshev@mail.ru>
2026-02-12 17:30:33 +0300

    доделал 4 степень и протестил

--- include/numAux.h
+++ include/numAux.h
@@ -45,11 +45,7 @@ using namespace std;
 
 namespace numAux {
     //возвращает знак числа
-    inline double sgn(double a) {
-        if (a > 0) return 1;
-        if (a < 0) return -1;
-        return 0;
-    }
+    #define sgn(x) (x > 0 ? 1 : x < 0 ? -1 : 0)
     //возвращает угол, приведенный к диапазону [-2pi, 2pi]
     double windDownAngle(double angle) {
         if (abs(angle) > 2 * M_PI)
@@ -67,7 +63,7 @@ namespace numAux {
         if (a == 0) {
             return 0;
         }
-        prod[0] = -a / b;
+        prod[0] = -b / a;
         return 1;
     }
     //решить уравнение 2 степени
@@ -85,38 +81,42 @@ namespace numAux {
         }
         prod[0] = (-b - sqrt(D)) / (2 * a);
         prod[1] = (-b + sqrt(D)) / (2 * a);
+        return 2;
     }
     //решить уравнение 3 степени
     int solveThree(double *prod, double a, double b, double c, double d) {
         if (a == 0) {
             return solveTwo(prod, b, c, d);
         }
-        double Q = (a * a - 3 * b) / 9;
-        double R = (2 * a * a * a - 9 * a * b + 27 * c) / 54;
+        b /= a;
+        c /= a;
+        d /= a;
+        double Q = (b * b - 3 * c) / 9;
+        double R = (2 * b * b * b - 9 * b * c + 27 * d) / 54;
         double S = Q * Q * Q - R * R;
         if (S > 0) {
             double phi = acos(R / sqrt(Q * Q * Q)) / 3;
-            prod[0] = -2 * sqrt(Q) * cos(phi - 2 * M_PI / 3) - a / 3;
-            prod[1] = -2 * sqrt(Q) * cos(phi) - a / 3;
-            prod[2] = -2 * sqrt(Q) * cos(phi + 2 * M_PI / 3) - a / 3;
+            prod[0] = -2 * sqrt(Q) * cos(phi - 2 * M_PI / 3) - b / 3;
+            prod[1] = -2 * sqrt(Q) * cos(phi) - b / 3;
+            prod[2] = -2 * sqrt(Q) * cos(phi + 2 * M_PI / 3) - b / 3;
             quicksort::quicksort(prod, 0, 3);
             return 3;
         }
         if (S < 0) {
             if (Q > 0) {
-                prod[0] = -2 * sgn(R) * sqrt(Q) * cosh(acosh(abs(R) / sqrt(Q * Q * Q)) / 3) - a / 3;
+                prod[0] = -2 * sgn(R) * sqrt(Q) * cosh(acosh(abs(R) / sqrt(Q * Q * Q)) / 3) - b / 3;
                 return 1;
             }
             if (Q < 0) {
-                prod[0] = -2 * sgn(R) * sqrt(-Q) * sinh(asinh(abs(R) / sqrt(-Q * Q * Q)) / 3) - a / 3;
+                prod[0] = -2 * sgn(R) * sqrt(-Q) * sinh(asinh(abs(R) / sqrt(-Q * Q * Q)) / 3) - b / 3;
                 return 1;
             }
-            prod[0] = -cbrt(c - a * a * a / 27) - a / 3;
+            prod[0] = -cbrt(d - b * b * b / 27) - b / 3;
             return 1;
         }
-        prod[0] = -2 * cbrt(R) - a / 3;
-        prod[1] = cbrt(R) - a / 3;
-        quicksort::quicksort(prod, 0, 2);
+        prod[0] = -2 * cbrt(R) - b / 3;
+        prod[1] = cbrt(R) - b / 3;
+        quicksort::quicksort(prod, 0, 3);
         return 2;
     }
     //решить уравнение 4 степени
@@ -128,30 +128,67 @@ namespace numAux {
         c /= a;
         d /= a;
         e /= a;
-        double A = -3 * b * b / 8 + c, B = b * b * b / 8 - b * c / 2 + d, C = -3 * b * b * b * b / 256 + c * b * b / 16 - b * d / 4 + e;
-        int nThree = solveThree(prod, 1, 5 * A / 2, 2 * A * A - C, A * A * A / 2 - A * C / 2 - B * B / 8);
-        double y = prod[0];
-        for (int i = 1; i < nThree; i++) {
-            if (prod[i] > y) {
-                y = prod[i];
-            }
-        }
-        int nRoots = 1;
+        double A = -3 * b * b / 8 + c, B = b * b * b / 8 - b * c / 2 + d, C = -3 * b * b * b * b / 256 + c * b * b / 16 - b * d / 4 + e, s1, s2;
         if (B == 0) {
             if (A * A < 4 * C) return 0;
             if (C == 0) {
                 prod[0] = -b / 4;
                 if (A < 0) {
-                    prod[1] = -b / 4 + sqrt(-A);
-                    prod[2] = -b / 4 - sqrt(-A);
+                    prod[0] = -b / 4 - sqrt(A);
+                    prod[1] = -b / 4;
+                    prod[2] = -b / 4 + sqrt(-A);
                     return 3;
                 }
                 return 1;
             }
             if (A < 0) {
-                double s1 = sqrt(A * A - 4 * C), s2;
-                s2 = sqrt(-A - s1);
+                s1 = sqrt(A * A - 4 * C);
+                s2 = sqrt((-A - s1) / 2);
+                prod[0] = -b / 4 - s2;
+                prod[1] = -b / 4 + s2;
+                s2 = sqrt((-A + s1) / 2);
+                prod[2] = -b / 4 - s2;
+                prod[3] = -b / 4 + s2;
+                quicksort::quicksort(prod, 0, 4);
+                return 4;
             }
+            return 0;
         }
+        double y = prod[solveThree(prod, 1, 5 * A / 2, 2 * A * A - C, A * A * A / 2 - A * C / 2 - B * B / 8) - 1];
+        double D = 256 * C * C * C - 128 * A * A * C * C + 144 * A * B * B * C - 27 * B * B * B * B + 16 * A * A * A * A * C - 4 * A * A * A * B * B;
+        s1 = sqrt(A + 2 * y);
+        s2 = -3 * A - 2 * y + 2 * abs(B) / s1;
+        if (s2 >= 0) {
+            s2 = sqrt(s2);
+            prod[0] = -b / 4 + (-sgn(B) * s1 - s2) / 2;
+            prod[1] = -b / 4 + (-sgn(B) * s1 + s2) / 2;
+        }
+        else {
+            return 0;
+        }
+        s2 = -3 * A - 2 * y - 2 * abs(B) / s1;
+        if (s2 >= 0) {
+            s2 = sqrt(s2);
+            prod[2] = -b / 4 + (sgn(B) * s1 - s2) / 2;
+            prod[3] = -b / 4 + (sgn(B) * s1 + s2) / 2;
+        }
+        else {
+            if (D == 0) {
+                return 1;
+            }
+            return 2;
+        }
+        quicksort::quicksort(prod, 0, 4);
+        if (D == 0) {
+            int idx = 0, dmin = prod[1] - prod[0];
+            for (int i = 1; i < 3; i++) {
+                if (prod[i + 1] - prod[i] < dmin) {
+                    idx = i;
+                }
+            }
+            double p = (prod[idx] + prod[idx + 1]) / 2;
+            return solveThree(prod, 1, b + p, c + b * p + p * p, d + c * p + b * p * p + p * p * p);
+        }
+        return 4;
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
+#include "include/numAux.h"
+#include "include/Timer.h"
 
 int main()
 {
+    double prod[4], time;
+    Timer timer;
+    timer.reset();
+    for (long i = 0; i < 1e6; i++) {
+        numAux::solveFour(prod, 1, -21, 148, -384, 256);
+    }
+    time = timer.time();
+    cout << time << endl;
     return 0;
 }
