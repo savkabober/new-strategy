c35405258ad7ffb7c9f1a0fb48dc7b94f9bf85e1
savkabober <savmalyshev@mail.ru>
2026-02-14 14:39:26 +0300

    метод ньютона

--- include/numAux.h
+++ include/numAux.h
@@ -222,4 +222,105 @@ namespace numAux {
         }
         return 4;
     }
+    double deltares(double *f, int n) {
+        static double summ;
+        summ = 0;
+        for(int i = 0; i < n; i++) {
+            summ += f[i] * f[i];
+        }
+        return sqrt(summ);
+    }
+    //решить систему уравнений методом гаусса
+    int gauss_sovle(double *x, double *a, double *b, int n) {
+        static double maxV, val, k;
+        static int maxJ, i1, i2, i, j, p;
+        maxV = 0;
+        maxJ = 0;
+        for(i = 0; i < n; i++) {
+            maxJ = 0;
+            maxV = 0;
+            for(j = 0; j < n - i; j++) {
+                val = abs(a[(i + j) * n + i]);
+                if(val > maxV) {
+                    maxV = val;
+                    maxJ = j;
+                }
+            }
+            if(maxV == 0) {
+                return 0;
+            }
+            if(maxJ != 0) {
+                i1 = (maxJ + i) * n + i;
+                i2 = i * n + i;
+                for(j = 0; j < n - i; j++) {
+                    a[i1 + j] += a[i2 + j];
+                    a[i2 + j] = a[i1 + j] - a[i2 + j];
+                    a[i1 + j] -= a[i2 + j];
+                }
+                b[maxJ + i] += b[i];
+                b[i] = b[maxJ + i] - b[i];
+                b[maxJ + i] -= b[i];
+            }
+            for(j = 1; j < n - i; j++) {
+                k = a[(i + j) * n + i] / a[i * n + i];
+                for(p = 1; p < n - i; p++) {
+                    a[(i + j) * n + i + p] -= a[i * n + i + p] * k;
+                }
+                b[i + j] -= b[i] * k;
+            }
+        }
+        for(i = n - 1; i >= 0; i--) {
+            for(j = n - 1; j > i; j--) {
+                b[i] -= a[i * n + j] * x[j];
+            }
+            if(a[i * n + i] == 0) {
+                x[i] = 0;
+            }
+            else {
+                x[i] = b[i] / a[i * n + i];
+            }
+        }
+        return 1;
+    }
+    //численно приблизить значение якобиана
+    void numJac(void (*f)(double*, double*, double*), double *x, double *jac, double *args, double *f0, int n, double d) {
+        static int i, j;
+        double f1[n];
+        f(x, args, f0);
+        for(i = 0; i < n; i++) {
+            x[i] += d;
+            f(x, args, f1);
+            x[i] -= d;
+            for(j = 0; j < n; j++) {
+                jac[j * n + i] = (f1[j] - f0[j]) / d;
+            }
+        }
+    }
+    //добавил метод ньютона для численного решения системы нелинейных уравнений
+    int newton(void (*jac)(void (*)(double*, double*, double*), double*, double*, double*, double*, int, double), 
+        void (*f)(double*, double*, double*), double *args, double *x, int n, double tol = 1e-7, int max_iter = 100, double d = 1e-7) {
+        static int i, j;
+        static bool flag;
+        double jacobian[n * n], fx[n], dx[n];
+        for(i = 0; i < max_iter; i++) {
+            jac(f, x, jacobian, args, fx, n, d);
+            flag = true;
+            for(j = 0; j < n; j++) {
+                if(abs(fx[j]) > tol) {
+                    flag = false;
+                }
+            }
+            if(flag) {
+                return 2;
+            }
+
+            if(gauss_sovle(dx, jacobian, fx, n) == 0) {
+                return 1;
+            }
+            for(j = 0; j < n; j++) {
+                x[j] -= dx[j]; 
+            }
+        }
+        return 0;
+    }
 }
